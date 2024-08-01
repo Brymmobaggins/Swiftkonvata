38c77682c1bc80624dd31337f8bc4cb04821417e
brymmobaggins <ibro2tablet21@yahoo.com>
2024-08-01 23:00:07 +0100

    update project name brand

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
-/** @type {import('tailwindcss').Config} */
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      karla: ["Karla", "sans-serif"],
+    },
+    colors: {
+      "light-wisteria": {
+        100: "#f2edfa",
+        200: "#e8def6",
+        300: "#d6c3ef",
+        400: "#c8ade8",
+        500: "#a376d6",
+        600: "#8c59c4",
+      },
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
