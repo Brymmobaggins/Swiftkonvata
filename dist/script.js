639e16a8f699f7623bf2328cda9798a5c8814e29
brymmobaggins <ibro2tablet21@yahoo.com>
2024-08-02 20:47:53 +0100

    update design

---
+++ dist/script.js
@@ -0,0 +1,25 @@
+/** @format */
+
+// const result = document.getElementById("result");
+const input = document.getElementById("amount");
+console.log(input);
+const currency = document.getElementById("currency");
+const result = document.getElementById("result");
+const convertButton = document.getElementById("convert");
+const settingsIcon = document.getElementById("settings-icon");
+const dropdown = document.getElementById("dropdown")
+const API_key = "";
+const API_URL = "";
+
+convertButton.addEventListener("click", () => {
+  convertRate();
+});
+
+function convertRate() {
+  const totalAmount = input.value * 5;
+  result.textContent = totalAmount;
+}
+
+settingsIcon.addEventListener("click", () => {
+  dropdown.classList.toggle("hidden")
+});
